4f5945ab5ff6440f1f52f735e0895e7ca0a10f64
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2022-04-20 12:15:09 +0200

    k

--- js/main.js
+++ js/main.js
@@ -1,47 +1,61 @@
-$(function() {
-    graphics_images = [
-        "2019_IF2",
-        "2018_IF1",
-        "2019_osiu1",
-        "2019_aedi_bbq",
-        "2018_mouette2",
-        "2017_mouette1",
-        "2018_ami2",
-        "2017_ami1",
-        "2016_montage_folklo",
-    ];
-    graphics_images.forEach(
-        function(image, i){
-            $("#swiper-graphics .swiper-wrapper").append(
-                "<img class='swiper-slide swiper-image' src='img/graphics/"+image+".png' class='carrousel-image'>"
+$(function(){
+    $("#leftColContent").load("leftColContent.html"); 
+    $("#rightColTab1").load("rightColTab1.html");
+
+    const promesseA = new Promise((resolve, reject) => {
+        $("#rightColTab2").load("rightColTab2.html", function() {
+            resolve( "Load was performed." );
+        });
+    });
+    promesseA.then(function(a){
+        graphics_images = [
+            "2019_IF2",
+            "2018_IF1",
+            "2019_AEDI",
+            "2019_osiu1",
+            "2019_aedi_bbq",
+            "2018_mouette2",
+            "2017_mouette1",
+            "2016_logo_mouette",
+            "2018_ami2",
+            "2017_ami1",
+            "2016_montage_folklo",
+        ];
+        graphics_images.forEach(
+            function(image){
+                $("#swiper-graphics .swiper-wrapper").append(
+                    "<img class='swiper-slide swiper-image' src='img/graphics/thumbnails/"+image+".jpg' class='carrousel-image'>"
+                );
+            }
+        );
+        for(var i = 0; i < 44; i++){
+            $("#swiper-photos .swiper-wrapper").append(
+                // "<div>"+
+                // "<a href='img/photography/fullsize/p-"+(i+1)+".jpg' target='_blank'>"+
+                "<img class='swiper-slide swiper-image' src='img/photography/thumbnails/p-"+(i+1)+".jpg' />"
+                // "</div>"
             );
         }
-    )
-})
 
-$(function() {
-    for(var i = 0; i < 44; i++){
-        $("#swiper-photos .swiper-wrapper").append(
-            "<img class='swiper-slide swiper-image' src='img/photography/thumbnails/p-"+(i+1)+".jpg'>"
-        );
-    }
-})
-
-const swiper = new Swiper('.swiper', {
-    cssMode: true,
-    loop:true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-    spaceBetween: 30,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: true,
-    // },
+        const swiper = new Swiper('.swiper', {
+            cssMode: true,
+            // loop:true,
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+              el: ".swiper-pagination",
+            },
+            mousewheel: true,
+            keyboard: true,
+            spaceBetween: 30,
+            centeredSlides: true,
+            // autoplay: {
+            //     delay: 2500,
+            //     disableOnInteraction: false,
+            // },
+        });
+    });
 });
+  
